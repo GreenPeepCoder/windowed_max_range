5d4235dabfeec1ed23d20a1f9d3cd316f0d60908
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-03-31 15:39:14 -0400

    Start Phase IV - Outline methods needed for StackQueue class

---
+++ lib/stackqueue.rb
@@ -0,0 +1,20 @@
+require 'mystack'
+
+class StackQueue
+    def initialize
+        @push_stack = MyStack.new
+        @pop_stack = MyStack.new
+    end
+
+    def size
+    end
+
+    def empty?
+    end
+
+    def enqueue
+    end
+
+    def dequeue
+    end
+end
