90c0f0379949755d77bc6b4c77d20df971c83201
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-04-02 14:17:59 -0400

    Start Phase V MinMaxStack - implement basic methods

---
+++ lib/minmaxstack.rb
@@ -0,0 +1,31 @@
+require_relative "mystack"
+
+class MinMaxStack
+    def initialize
+        @store = MyStack.new
+    end
+
+    def peek
+        @store.peek
+    end
+
+    def size
+        @store.size
+    end
+
+    def empty?
+        @store.empty?
+    end
+
+    def max
+    end
+
+    def min
+    end
+
+    def pop
+    end
+
+    def push(ele)
+    end
+end
